531660129ef777a02672e43a9fc479e8e5908590
klkirstyleeskl <k.lees@btinternet.com>
2019-01-17 15:01:06 +0000

    completes MVP

--- src/views/list_view.js
+++ src/views/list_view.js
@@ -1,20 +1,26 @@
-const PubSub = require('../helpers/pub_sub.js')
+const PubSub = require('../helpers/pub_sub.js');
+const MunroView = require('./munro_view.js');
 
 
-const ListView = function(element) {
-  this.element = element
+const ListView = function(container) {
+  this.container = container
 };
 
 
 ListView.prototype.bindEvents = function () {
   PubSub.subscribe('Munro:munro-data-loaded', (event) => {
-    const munro = event.detail;
-    console.log(munro);
+     this.munro = event.detail;
+    this.render()
   })
-
 };
 
-
+ListView.prototype.render = function () {
+  this.munro.forEach((munro_selected) => {
+    const munroView = new MunroView(this.container, munro_selected);
+    console.log();
+    munroView.render();
+  })
+};
 
 
 
---
+++ src/views/munro_view.js
@@ -0,0 +1,45 @@
+const MunroView = function(container, munro) {
+  this.munroContainer = container;
+  this.munro = munro;
+};
+
+MunroView.prototype.render = function () {
+  const munroContainer = document.createElement('div');
+  munroContainer.classList.add('munro');
+
+  const name = this.createMunroHeading();
+  munroContainer.appendChild(name);
+
+  const infoList = this.createInfoList();
+  munroContainer.appendChild(infoList);
+
+  this.munroContainer.appendChild(munroContainer);
+};
+
+MunroView.prototype.createMunroHeading = function () {
+  const name = document.createElement('h2');
+  name.classList.add('munro-name');
+  name.textContent = this.munro.name;
+  console.log(name);
+  return name;
+};
+
+MunroView.prototype.createInfoList = function () {
+  const infoList = document.createElement('ul');
+  infoList.classList.add('munro-list');
+  this.populateList(infoList);
+  return infoList;
+};
+
+MunroView.prototype.populateList = function (list) {
+  const munroListItem = document.createElement('li');
+  munroListItem.textContent = this.munro.meaning;
+  console.log(this.munro.meaning);
+  list.appendChild(munroListItem);
+  const munroListItemHeight = document.createElement('li');
+  munroListItemHeight.textContent = this.munro.height
+  list.appendChild(munroListItemHeight);
+};
+
+
+module.exports = MunroView;
